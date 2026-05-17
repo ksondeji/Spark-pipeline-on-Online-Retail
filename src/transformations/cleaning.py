e9c9adcd49c7004fe80601b34c57eab02aa1c102
Karl <karl.sondeji@gmail.com>
2026-05-17 20:58:45 +0200

    Enhance data cleaning function with additional filters and type casting
    - Updated the clean_transactions function to filter out null values for CustomerID, UnitPrice, Quantity, and InvoiceNo.
    - Added filters to exclude InvoiceNo starting with 'c' and the specific InvoiceNo '541431'.
    - Ensured StockCode length is 5 and cast Quantity and UnitPrice to appropriate types.
    - Converted InvoiceDate to timestamp format for better processing.

--- src/transformations/cleaning.py
+++ src/transformations/cleaning.py
@@ -1,6 +1,17 @@
-def clean_transactions(df):
+from pyspark.sql.functions import col, lower, to_timestamp
 
+def clean_transactions(df):
     return (
-        df.filter(...)
-          .withColumn(...)
+        df.filter(col("CustomerID").isNotNull())
+        .filter(col("UnitPrice").isNotNull())
+        .filter(col("Quantity").isNotNull())
+        .filter(col("InvoiceNo").isNotNull())
+        # DELETE ... WHERE lower(InvoiceNo) LIKE 'c%'
+        .filter(~lower(col("InvoiceNo")).startswith("c"))
+        # DELETE ... WHERE InvoiceNo = '541431'
+        .filter(col("InvoiceNo") != "541431")
+        .filter(col("StockcCode").length() == 5)
+        .withColumn("Quantity", col("Quantity").cast("int"))
+        .withColumn("UnitPrice", col("UnitPrice").cast("double"))
+        .withColumn("InvoiceDate", to_timestamp(col("InvoiceDate"), "dd/MM/yyyy HH:mm:ss"))
     )
