d36879769112b946d644bdc61ba0a2a806d69dfa
Karl <karl.sondeji@gmail.com>
2026-05-17 20:58:33 +0200

    Add initial file and update Spark session creation comment
    - Created __init__.py as the package initializer.

--- src/ingestion/spark_session.py
+++ src/ingestion/spark_session.py
@@ -1,7 +1,7 @@
 from pyspark.sql import SparkSession
 
 def create_spark_session():
-
+#"""Extrait exactement le bloc de création de SparkSession de ton notebook. La config Delta Lake, les jars, tout. Prend la config en paramètre depuis config.py"""
     spark = (
         SparkSession.builder
         .appName("online-retail-pipeline")
